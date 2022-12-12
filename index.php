7ab49ba005d650f2d6614b92f5254b04ef61bc42
SevenErol <lorenzo.maggi1998@gmail.com>
2022-12-12 16:54:25 +0100

    fixed style and created folder for models

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-require __DIR__ . './Product.php';
+require __DIR__ . './Models/Product.php';
 
-require __DIR__ . './Category.php';
+require __DIR__ . './Models/Category.php';
 
 $products_dog = [
     new Product('https://picsum.photos/400/200', 'bowl', 'Multifunctional bowl for food and water', true, '24.99£', new Category('objects'), 7),
@@ -54,58 +54,66 @@ $products_cat = [
 
 
     <main>
-        <div class="container">
+        <div class="container p-3">
+
+            <h2 class="text-center">Dogs Products</h2>
 
             <div class="row p-5 align-items-stretch">
 
                 <?php foreach ($products_dog as $key => $product) : ?>
                     <div class="col-4">
 
-                        <div class="card justify-content-between" style="height: 100%">
+                        <div class="card" style="height: 100% ;">
                             <img class="img-fluid" src="<?php echo $product->image ?>" alt="">
 
-                            <div class="card-body">
+                            <div class="card-body d-flex flex-column justify-content-between">
 
                                 <h4 class="text-center mb-5"><?php echo $product->name ?></h4>
 
                                 <p><strong>Description: </strong><?php echo $product->description ?></p>
 
-                                <?php if ($product->availability) : ?>
+                                <div class="d-flex flex-column">
 
-                                    <div class="d-flex align-items-center">
+                                    <?php if ($product->availability) : ?>
 
-                                        <p class="m-0 p-0">The product is available now</p>
+                                        <div class="d-flex align-items-center">
 
-                                        <i class="fa-solid fa-circle green ms-2"></i>
+                                            <p class="m-0 p-0">The product is available now</p>
+
+                                            <i class="fa-solid fa-circle green ms-2"></i>
+
+                                        </div>
+
+
+
+                                    <?php else : ?>
+
+                                        <div class="d-flex align-items-center">
+
+                                            <p class="m-0 p-0">We are sorry the product is not available</p>
+
+                                            <i class="fa-solid fa-circle red ms-2"></i>
+
+                                        </div>
+
+
+                                    <?php endif; ?>
+
+                                    <div>
+                                        <strong><?php echo $product->price ?></strong>
+                                    </div>
+
+                                    <div class="text-center">
+
+                                        <i class="fa-solid fa-dog"></i>
 
                                     </div>
 
+                                    <p class="category text-center mt-3"><strong>Category: </strong><?php echo $product->category->type ?></p>
 
-
-                                <?php else : ?>
-
-                                    <div class="d-flex align-items-center">
-
-                                        <p class="m-0 p-0">We are sorry the product is not available</p>
-
-                                        <i class="fa-solid fa-circle red ms-2"></i>
-
-                                    </div>
-
-
-                                <?php endif; ?>
-
-                                <div>
-                                    <strong><?php echo $product->price ?></strong>
-                                </div>
-
-                                <div class="text-center">
-
-                                    <i class="fa-solid fa-dog"></i>
 
                                 </div>
 
-                                <p class="category text-center mt-3"><strong>Category: </strong><?php echo $product->category->type ?></p>
 
                             </div>
 
