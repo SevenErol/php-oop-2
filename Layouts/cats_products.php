93a1eb772ceec5ce7c1f6e4ebc054e02c9232a38
SevenErol <lorenzo.maggi1998@gmail.com>
2022-12-12 17:04:50 +0100

    refactoring with folder

---
+++ Layouts/cats_products.php
@@ -0,0 +1,72 @@
+<div class="container p-3">
+
+    <h2 class="text-center">Cats Products</h2>
+
+    <div class="row p-5 align-items-stretch">
+
+        <?php foreach ($products_cat as $key => $product) : ?>
+            <div class="col-4">
+
+                <div class="card" style="height: 100% ;">
+                    <img class="img-fluid" src="<?php echo $product->image ?>" alt="">
+
+                    <div class="card-body d-flex flex-column justify-content-between">
+
+                        <h4 class="text-center mb-5"><?php echo $product->name ?></h4>
+
+                        <p><strong>Description: </strong><?php echo $product->description ?></p>
+
+                        <div class="d-flex flex-column">
+
+                            <?php if ($product->availability) : ?>
+
+                                <div class="d-flex align-items-center">
+
+                                    <p class="m-0 p-0">The product is available now</p>
+
+                                    <i class="fa-solid fa-circle green ms-2"></i>
+
+                                </div>
+
+
+
+                            <?php else : ?>
+
+                                <div class="d-flex align-items-center">
+
+                                    <p class="m-0 p-0">We are sorry the product is not available</p>
+
+                                    <i class="fa-solid fa-circle red ms-2"></i>
+
+                                </div>
+
+
+                            <?php endif; ?>
+
+                            <div>
+                                <strong><?php echo $product->price ?></strong>
+                            </div>
+
+                            <div class="text-center">
+
+                                <i class="fa-solid fa-cat"></i>
+
+                            </div>
+
+                            <p class="category text-center mt-3"><strong>Category: </strong><?php echo $product->category->type ?></p>
+
+
+                        </div>
+
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        <?php endforeach ?>
+
+    </div>
+
+</div>
--- index.php
+++ index.php
@@ -19,121 +19,15 @@ $products_cat = [
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>E-commerce pets products</title>
+require __DIR__ . "./Layouts/head.php";
 
-    <style>
-        .red {
-            color: red;
-        }
+require __DIR__ . "./Layouts/dogs_products.php";
 
-        .green {
-            color: green;
-        }
+require __DIR__ . "./Layouts/cats_products.php";
 
-        .fa-dog {
-            font-size: 36px;
-        }
-    </style>
+require __DIR__ . "./Layouts/footer.php"
 
-    <!-- link fontawesome -->
-    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css' integrity='sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==' crossorigin='anonymous' referrerpolicy='no-referrer' />
+?>
 
-    <!-- link bootstrap  -->
-    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT' crossorigin='anonymous'>
-
-</head>
-
-<body>
-
-
-    <main>
-        <div class="container p-3">
-
-            <h2 class="text-center">Dogs Products</h2>
-
-            <div class="row p-5 align-items-stretch">
-
-                <?php foreach ($products_dog as $key => $product) : ?>
-                    <div class="col-4">
-
-                        <div class="card" style="height: 100% ;">
-                            <img class="img-fluid" src="<?php echo $product->image ?>" alt="">
-
-                            <div class="card-body d-flex flex-column justify-content-between">
-
-                                <h4 class="text-center mb-5"><?php echo $product->name ?></h4>
-
-                                <p><strong>Description: </strong><?php echo $product->description ?></p>
-
-                                <div class="d-flex flex-column">
-
-                                    <?php if ($product->availability) : ?>
-
-                                        <div class="d-flex align-items-center">
-
-                                            <p class="m-0 p-0">The product is available now</p>
-
-                                            <i class="fa-solid fa-circle green ms-2"></i>
-
-                                        </div>
-
-
-
-                                    <?php else : ?>
-
-                                        <div class="d-flex align-items-center">
-
-                                            <p class="m-0 p-0">We are sorry the product is not available</p>
-
-                                            <i class="fa-solid fa-circle red ms-2"></i>
-
-                                        </div>
-
-
-                                    <?php endif; ?>
-
-                                    <div>
-                                        <strong><?php echo $product->price ?></strong>
-                                    </div>
-
-                                    <div class="text-center">
-
-                                        <i class="fa-solid fa-dog"></i>
-
-                                    </div>
-
-                                    <p class="category text-center mt-3"><strong>Category: </strong><?php echo $product->category->type ?></p>
-
-
-                                </div>
-
-
-                            </div>
-
-                        </div>
-
-                    </div>
-
-                <?php endforeach ?>
-
-            </div>
-
-        </div>
-
-    </main>
-
-
-
-    <!-- script bootstrap -->
-    <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js' integrity='sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8' crossorigin='anonymous'></script>
-
-</body>
-
-</html>
